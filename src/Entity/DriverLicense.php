91961594b75ffb06777ad258d9dde5c3674589b6
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2022-01-20 20:14:35 +0100

    VO 2.1 - DriverLicense - wprowadzenie obiektu z testami jednostkowymi

---
+++ src/Entity/DriverLicense.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LegacyFighter\Cabs\Entity;
+
+class DriverLicense
+{
+    public const DRIVER_LICENSE_REGEX = '/^[A-Z9]{5}\d{6}[A-Z9]{2}\d[A-Z]{2}$/';
+
+    private string $driverLicense;
+
+    private function __construct(string $driverLicense)
+    {
+        $this->driverLicense = $driverLicense;
+    }
+
+    public static function withLicense(string $driverLicense): self
+    {
+        if($driverLicense === '' || preg_match(self::DRIVER_LICENSE_REGEX, $driverLicense) !== 1) {
+            throw new \InvalidArgumentException('Illegal license no = '.$driverLicense);
+        }
+
+        return new self($driverLicense);
+    }
+
+    public static function withoutValidation(string $driverLicense): self
+    {
+        return new self($driverLicense);
+    }
+
+    public function asString(): string
+    {
+        return $this->driverLicense;
+    }
+}
---
+++ tests/Unit/Entity/DriverLicenseTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LegacyFighter\Cabs\Tests\Unit\Entity;
+
+use LegacyFighter\Cabs\Entity\DriverLicense;
+use PHPUnit\Framework\TestCase;
+
+class DriverLicenseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function cannotCreateInvalidLicense(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        DriverLicense::withLicense('invalid');
+    }
+
+    /**
+     * @test
+     */
+    public function cannotCreateEmptyLicense(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        DriverLicense::withLicense('');
+    }
+
+    /**
+     * @test
+     */
+    public function canCreateValidLicense(): void
+    {
+        //when
+        $license = DriverLicense::withLicense('FARME100165AB5EW');
+
+        //then
+        self::assertEquals('FARME100165AB5EW', $license->asString());
+    }
+
+    /**
+     * @test
+     */
+    public function canCreateInvalidLicenseExplicitly(): void
+    {
+        //when
+        $license = DriverLicense::withoutValidation('invalid');
+
+        //then
+        self::assertEquals('invalid', $license->asString());
+    }
+}
