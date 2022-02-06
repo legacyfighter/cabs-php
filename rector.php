e65f6490d6c24a4fc65c73948a306ddfd134bb91
wiktorsadowski <wiktor.sadowski@landingi.com>
2022-02-06 22:25:00 +0100

    Configured rector.

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PATHS, [__DIR__.'/src', __DIR__.'/tests']);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+
+    $containerConfigurator->import(SymfonySetList::SYMFONY_53);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+};
